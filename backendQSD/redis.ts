1d24c8e4b5214a870eeaa0eeb4833d8ab7dad3a3
misba2002 <misbajunneidi@gmail.com>
2026-01-27 11:24:37 +0530

    updated tls in redis.ts required for upstash

--- backendQSD/redis.ts
+++ backendQSD/redis.ts
@@ -7,6 +7,8 @@ export const redis=new IORedis(
     {
         host:process.env.REDIS_HOST,
         port:process.env.REDIS_PORT ? Number(process.env.REDIS_PORT) : 6379,
+        password: process.env.REDIS_PASSWORD,
+        tls: {},  
          maxRetriesPerRequest: null,
     }
 );
